ba25911d9e3bf4b9fa99361250c89981304fc14b
jmariel <jmarielgaddi@gmail.com>
2018-06-19 23:03:14 +0200

    added attivitàForm.html in the project

--- SIW-PROGETTO-2018/src/main/java/it/uniroma3/siw/repository/AttivitàRepository.java
+++ SIW-PROGETTO-2018/src/main/java/it/uniroma3/siw/repository/AttivitàRepository.java
@@ -10,5 +10,6 @@ import it.uniroma3.siw.model.Attività;
 public interface AttivitàRepository extends CrudRepository<Attività, Long> {
 	public List<Attività> findByNomeAndDataEora (String nome, Date dataEora);
 	public List<Attività> findByNome(String nome);
+	public List<Attività> findByDataEora(Date dataEora);
 
 }
